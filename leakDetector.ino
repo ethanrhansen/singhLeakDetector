bbe397c53b3655643730af983e304652c70c4155
ethanrhansen <77511493+ethanrhansen@users.noreply.github.com>
2021-02-03 15:21:55 -0700

    Update leakDetector.ino

--- leakDetector.ino
+++ leakDetector.ino
@@ -1,42 +1,34 @@
 #include <ESP8266WiFi.h>
 
+//Connecting to wifi and setting up the device with your IFTTT account
+const char* ssid     = "CSMwireless"; //The ssid name for the wifi network for the device to connect to
+const char* password = ""; //Password to the wifi network. 
+
+const char* host = "wifitest.adafruit.com";
+
 
 //Ohmmeter vars
 int analogPin= A0; //The analog pin that we read the voltage from the "ohmmeter" circuit is measured.
- //For the Feather Huzzah ESP8266, the max voltage on the analog pin is 1 V, thus the two resistor values in the circuit must be such that the output voltage will not exceed 1 V.
 int Vin= 3.3; //input voltage to the circuit, from the voltage pin on the ESP8266
-float Rk= 10000;//known resistor value
-float Rthresh = 70000;//threshold resistance measurement to send a notification
+float Rk= 10000;//known resistor value (in ohms)
+float Rthresh = 70000;//threshold resistance, below which a notification should be sent
 
-float raw= 0; //placeholder variable used in the resistance calculation
+float Vread= 0; //placeholder variable used in the resistance calculation
 float Vout= 0; //initialize variable for the measured voltage on the analog pin 
 float Rm= 0; //initialize measured resitance variable
-float buffer= 0; //placeholder variable for voltage calculations
 float avgCount= 0; //initialize loop counting varaible for taking the average resistance
 float Rsum= 0; //intialize summing varaible for average
 float Ravg= 0; //intialize variable for the average resistance over 10 measurements
 
 
-//notification variables
-bool notified= false;//checks if already sent notification
-float count= 0;//counter for how many iterations the counter has gone through without sending a notification
-
-
-//Connecting to wifi and setting up the device with your IFTTT account
-const char* ssid     = "CSMwireless"; //The ssid name for the wifi network for the device to connect to
-const char* password = ""; //Password to the wifi network. For school networks, you must register the device with its MAC ID online, and then leave the password blank.
-
-const char* host = "wifitest.adafruit.com";
-
 //links for connecting to IFTTT
 //Unique Webhooks Key
-String webKey = "bCwCwcKX5araHKmghwTMT"; 
+String webKey = "REPLACE_WITH_WEBHOOKS_KEY"; 
 
 //Trigger names
 String logTrig = "data_log"; //Name of your applet for logging data to a google sheets file
-String notifTrig = "my_ESP8266"; //Name of your applet for sending a phone notification
+String notifTrig = "send_notif"; //Name of your applet for sending a phone notification
 String mailTrig = "send_mail"; //Name of your applet for sending an email
-
 
 // unique IFTTT URL resource, the links to the IFTTT events that you want to trigger. Nothing to updated
 const char* logDat = String("/trigger/" + logTrig + "/with/key/" + webKey).c_str();
@@ -46,6 +38,9 @@ const char* mailSend = String("/trigger/" + mailTrig + "/with/key/" + webKey).c_
 // Maker Webhooks IFTTT: The link for trigger the online event
 const char* server = "maker.ifttt.com";
 
+//notification variables
+bool notified= false;//checks if already sent notification
+float count= 0;//counter for how many iterations the counter has gone through without sending a notification
 
 
 void setup() {
@@ -82,16 +77,11 @@ void loop() {
 
 //Ohmmeter Function
 
-  raw= analogRead(analogPin); //read the analog pin. the readout is 1024*voltage (because the max voltage is 1V. If max was 3.3 V, then it would be voltage*1024/3.3
-  if(raw) 
+  vRead= analogRead(analogPin); //read the analog pin. the readout is 1024*voltage (because the max voltage is 1V. If max was 3.3 V, then it would be voltage*1024/3.3
+  if(vRead) 
   {
-    Vout= (raw)/1024.0; //the readout from the analog pin is 
-    buffer= (Vin/Vout) -1;
-    Rm= Rk * buffer; //solve for the resistance from the voltage reading and the known resistor value
-    //Serial.print("Vout: ");
-    //Serial.println(Vout);
-    //Serial.print("Rm: ");
-    //Serial.println(Rm);
+    Vout= (vRead)/1024.0; //the readout from the analog pin is 
+    Rm = Rk * ((Vin/Vout) -1); //solve for the resistance from the voltage reading and the known resistor value
     delay(500);
   }
   //Take the average of the resitances over 10 measurements
